ef40ca30d51f0b091dc4f9aad6159d029e5b1412
Chu Sojin <chu3322@kumoh.ac.kr>
2025-08-25 22:29:29 +0900

    Update missionpage.jsx

--- frontend/src/pages/mission/missionpage.jsx
+++ frontend/src/pages/mission/missionpage.jsx
@@ -126,6 +126,7 @@ export default function MissionPage() {
     const fileInput = document.createElement("input");
     fileInput.type = "file";
     fileInput.accept = "image/*";
+    fileInput.capture = "environment";
     fileInput.click();
 
     fileInput.onchange = async () => {
